92ee1b14adb0786622019c20f9c24f8d0b7335ad
codesonu1 <tsonukumar300@gmail.com>
2023-11-28 12:14:26 +0545

    ltd

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -4,24 +4,25 @@ import slide1 from "../assets/slide1.png";
 import img1 from "../assets/1.png";
 import img2 from "../assets/img2.png";
 import video from "../assets/video.mp4";
+import video2 from "../assets/video2.mp4";
 
 import "../index.css";
 import VideoPlayer from "./VideoTag";
 import { Link } from "react-router-dom";
 
 const slides = [
-  // {
-  //   image: "https://www.mainawatisteel.com/images/banner2.jpg",
-  //   caption: "Image 3 Caption",
-  //   subtitle: "Subtitle 3",
-  // },
   {
-    image: { video },
+    media: video,
     caption: "Building nation",
     subtitle: "Always a step ahead in",
     type: "video",
   },
-
+  {
+    media: video2,
+    caption: "Building nation",
+    subtitle: "Always a step ahead in",
+    type: "video",
+  },
   // Add more slides here
 ];
 
@@ -40,36 +41,44 @@ function Slideshow() {
     );
   };
 
-  // Automatically transition to the next slide every 3 seconds
   useEffect(() => {
     const intervalId = setInterval(nextSlide, 3000);
 
-    // Clear the interval when the component unmounts
     return () => clearInterval(intervalId);
   }, []);
 
   const currentSlide = slides[currentSlideIndex];
 
-  return (
-    <div className="relative">
+  const renderMedia = () => {
+    // if (currentSlide.type === "video") {
+    return (
       <video
-        className="w-full h-full "
+        className="w-full h-full"
         controls
         loop
         autoPlay
         muted
-        src={video}
+        src={currentSlide.media}
         type="video/mp4"
       ></video>
+    );
+    // } else if (currentSlide.type === "image") {
+    //   return <img src={currentSlide.media} alt={currentSlide.caption} />;
+    // }
+  };
 
-      <div className="absolute inset-0 flex flex-col  mx-8   justify-center">
+  return (
+    <div className="relative">
+      {renderMedia()}
+
+      <div className="absolute inset-0 flex flex-col mx-[6rem] text-center  justify-center">
         <div className="text-white text-start">
-          <span className="text-4xl font-bold bg-[white] p-4 rounded-md  text-primary uppercase ">
+          <span className="text-4xl font-bold bg-[white] p-4 rounded-md text-primary uppercase">
             {currentSlide.caption}
           </span>
-          <br></br>
+          <br />
           <p className="my-8">
-            <span className="text-xl px-4 rounded-md bg-secondery text-[black] py-4 ">
+            <span className="text-xl px-4 rounded-md bg-secondery text-[black] py-4">
               {currentSlide.subtitle}
             </span>
           </p>
@@ -82,18 +91,19 @@ function Slideshow() {
           </p>
         </div>
       </div>
-      {/* <button
+
+      <button
         onClick={prevSlide}
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full md:mt-[4rem] lg:mt-0 mt-[0rem] "
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full md:mt-[4rem] lg:mt-0 mt-[0rem]"
       >
-        <BiSkipPrevious className="bg-secondery  text-3xl rounded-full hover:text-primary" />
+        <BiSkipPrevious className="bg-secondery text-3xl rounded-full hover:text-primary" />
       </button>
       <button
         onClick={nextSlide}
         className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full md:mt-[4rem] lg:mt-0 mt-[0rem]"
       >
-        <BiSkipNext className="bg-secondery  text-3xl rounded-full hover:text-primary" />
-      </button> */}
+        <BiSkipNext className="bg-secondery text-3xl rounded-full hover:text-primary" />
+      </button>
     </div>
   );
 }
